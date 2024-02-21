3f6b181b6b7cbe9783fc87c08a04d03582a11346
AhsanHasan <s.ahsan_hasan@hotmail.com>
2024-02-21 17:56:37 +0000

    env file for production

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
-    apiBase: 'http://localhost:3000/api',
+    apiBase: 'http://159.89.1.194:3001/api',
     googleClientId: '864601846719-repk30g6m4m26oe69n7tkckgaulpknt0.apps.googleusercontent.com',
+    cookieDomain: 'localhost',
     versionControl: {
         env: 'local',
         v: '1.0.0'
@@ -13,5 +14,9 @@ export const environment = {
         storageBucket: "chatteree-414221.appspot.com",
         messagingSenderId: "864601846719",
         appId: "1:864601846719:web:4f11cfa36313ec72c1670a"
+    },
+    pusher: {
+        key: '1aef33faf3997c780653',
+        cluster: 'eu'
     }
 };
