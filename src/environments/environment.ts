2468a39701034d3f1ac7344e5f5a7614ecf13324
AhsanHasan <s.ahsan_hasan@hotmail.com>
2024-02-25 23:20:36 +0000

    removed keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,22 @@
 export const environment = {
     production: true,
     apiBase: 'http://159.89.1.194:3001/api',
-    googleClientId: '864601846719-repk30g6m4m26oe69n7tkckgaulpknt0.apps.googleusercontent.com',
+    googleClientId: '',
     cookieDomain: '159.89.1.194',
     versionControl: {
         env: 'local',
         v: '1.0.0'
     },
     firebaseConfig: {
-        apiKey: "AIzaSyC8JAFbnBVurNptHeF4OsEM52DVJfaBUkI",
-        authDomain: "chatteree-414221.firebaseapp.com",
-        projectId: "chatteree-414221",
-        storageBucket: "chatteree-414221.appspot.com",
-        messagingSenderId: "864601846719",
-        appId: "1:864601846719:web:4f11cfa36313ec72c1670a"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
     },
     pusher: {
-        key: '1aef33faf3997c780653',
+        key: '',
         cluster: 'eu'
     }
 };
